9905ebb5acd5d397b817e81713f7d804ae39c1a6
unknown <matteoalberta@live.de>
2021-11-25 23:29:58 +0100

    Initial Commit

--- pages/news/images.js
+++ pages/news/images.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 import { Fragment } from 'react';
 import Link from 'next/link';
 
-export const getServerSideProps = async ({}) => {
+export async function getServerSideProps() {
 
 	const res = await fetch("https://mypaintings9-admin.herokuapp.com/images");
 	const data = await res.json();
